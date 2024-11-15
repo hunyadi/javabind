93f7326b7bfa72be735d63e674c6152c8a38daa2
Levente Hunyadi <levente.hunyadi@instructure.com>
2024-11-15 15:08:36 +0100

    Add GitHub Actions build script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 set -e
 find java -name "*.java" > build/sources.txt
+mkdir -p jar
 javac -d jar -cp java @build/sources.txt
 java -Djava.library.path=build -cp jar -ea hu.info.hunyadi.test.TestJavaBind
